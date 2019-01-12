13f3d6b8b59ac9157f5d0fb5f8363313715a96ee
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-12 23:07:42 +0700

    Update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+	$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+
 require __DIR__."/../bootstrap/web_init.php";
 
 (new Main)->run();
